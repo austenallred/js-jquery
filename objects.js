2c32322805b7ec4d66a085b38f76c62ee34ed1e0
Austen Allred <austen.allred@gmail.com>
2014-09-15 13:15:38 -0600

    initial commit

---
+++ objects.js
@@ -0,0 +1,10 @@
+var jim = {
+  name: "Jim", 
+  skills: ["JavaScript", "Ruby", "Dancing"],
+  "favorite color": "blue",
+};
+
+jim["favorite color"] = "green";
+
+console.log(jim["name"]);
+console.log(jim["favorite color"]);
